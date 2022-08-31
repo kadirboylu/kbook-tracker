c9cf43964e5885aede48ccf9559bfe0f03a33974
kadirboylu <kadir.boylu@outlook.com>
2022-08-31 21:16:35 +0300

    Add folders

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from "./Header";
+export * from "./Button";
+export * from "./Theme";
+export * from "./Footer";
+export * from "./Layout";
+export * from "./Search";
+export * from "./Input";
