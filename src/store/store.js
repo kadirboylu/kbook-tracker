82bd0d5597afe8f155093e655f33008dc534f901
kadirboylu <kadir.boylu@outlook.com>
2022-09-03 21:16:50 +0300

    Add authReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import bookReducer from "./bookSlice";
+import authReducer from "./authSlice";
 
 export const store = configureStore({
   reducer: {
     book: bookReducer,
+    auth: authReducer,
   },
 });
