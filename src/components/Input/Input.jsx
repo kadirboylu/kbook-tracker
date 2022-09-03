ab833c5374434830deac48e092e58b6d3c00fe14
kadirboylu <kadir.boylu@outlook.com>
2022-09-03 21:15:00 +0300

    Update input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import clsx from "clsx";
 
 import styles from "./Input.module.scss";
 
-export const Input = ({ title, type, placeholder }) => {
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+
+export const Input = ({
+  id,
+  name,
+  placeholder,
+  title,
+  type,
+  value,
+  setValue,
+  error,
+}) => {
+  const [showPassword, setShowPassword] = useState(type);
+
   const extraStyles = {
     input: "dark:bg-slate-700 dark:text-white",
+  };
+
+  const handlePassword = () => {
+    showPassword === "password"
+      ? setShowPassword("text")
+      : setShowPassword("password");
   };
 
   return (
     <label className={styles.base}>
       <span className="dark:text-slate-800">{title}</span>
-      <input
-        className={extraStyles.input}
-        type={type}
-        placeholder={placeholder}
-      />
+      <div className="w-full relative">
+        <input
+          className={extraStyles.input}
+          id={id}
+          name={name}
+          type={showPassword}
+          value={value}
+          onChange={setValue}
+          placeholder={placeholder}
+        />
+        {type === "password" && (
+          <div
+            onClick={handlePassword}
+            className={clsx(styles.seePassword, "dark:text-slate-400")}
+          >
+            {showPassword === "password" ? (
+              <BsFillEyeFill />
+            ) : (
+              <BsFillEyeSlashFill />
+            )}
+          </div>
+        )}
+      </div>
+      {error && value && (
+        <span className="text-sm mt-2 ml-1 text-slate-600">• {error}</span>
+      )}
     </label>
   );
 };
