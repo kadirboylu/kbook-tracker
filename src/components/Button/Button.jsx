2fb963c53bd11052b5e7a93cc200cb03ad07faf6
kadirboylu <kadir.boylu@outlook.com>
2022-09-03 21:10:54 +0300

    Update button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 import styles from "./Button.module.scss";
 
-export const Button = ({ text, theme, type }) => {
+export const Button = ({ click, text, theme, type, disabled }) => {
+  const btnDisabled = disabled
+    ? "opacity-75 cursor-not-allowed hover:opacity-75 active:translate-y-0"
+    : "";
+
   return (
-    <button type={type} className={`${styles.base} ${styles[theme]}`}>
+    <button
+      onClick={click}
+      disabled={disabled}
+      type={type}
+      className={`${styles.base} ${styles[theme]} ${btnDisabled}`}
+    >
       {text}
     </button>
   );
