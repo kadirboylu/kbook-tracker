5eb86d72578449b6d5b49e90cb0f8dd13022c7bb
kadirboylu <kadir.boylu@outlook.com>
2022-09-03 21:17:28 +0300

    Add name input

--- src/routes/Signup/Signup.jsx
+++ src/routes/Signup/Signup.jsx
@@ -2,21 +2,12 @@ import React from "react";
 
 import styles from "./Signup.module.scss";
 
-import { Input, Button } from "@/components";
+import { RegisterForm } from "@/components";
 
 export const Signup = () => {
-  const extraStyles = {
-    form: "dark:border-slate-700",
-  };
-
   return (
     <div className={styles.base}>
-      <form className={extraStyles.form}>
-        <Input title="Name" type="text" placeholder="Name" />
-        <Input title="Email" type="text" placeholder="Email" />
-        <Input title="Password" type="password" placeholder="Password" />
-        <Button text="SIGN UP" theme="primary" type="submit" />
-      </form>
+      <RegisterForm />
     </div>
   );
 };
