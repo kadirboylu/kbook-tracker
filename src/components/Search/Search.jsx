2452f393c4fc99457c7bf6ea6aef10b32df9ba93
Kadir Boylu <kadir.boylu@outlook.com>
2022-09-07 00:10:07 +0300

    Add loadash.debounce

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import clsx from "clsx";
+import debounce from "lodash.debounce";
 import { FaSearch } from "react-icons/fa";
+import { setQuery, reset } from "@/store/bookSlice";
 
 import styles from "./Search.module.scss";
 
 export const Search = () => {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+
   const extraStyles = {
     dark: "dark:bg-slate-700 dark:text-white",
   };
 
+  useEffect(() => {
+    dispatch(reset()); // reset the characters, queryResult and offset when the query changes
+    dispatch(setQuery(text.replace(/\s/g, "+")));
+  }, [text]);
+
+  const updateQuery = (e) => setText(e.target.value); // update the query when the user types in the search bar
+
+  const debouncedOnChange = debounce(updateQuery, 500); // debounce the search input to prevent unnecessary requests
+
   return (
     <div className={styles.base}>
-      <input type="text" placeholder="Search" className={extraStyles.dark} />
+      <input
+        type="text"
+        placeholder="Search"
+        className={extraStyles.dark}
+        onChange={debouncedOnChange}
+      />
       <div className={clsx(styles.icon, extraStyles.dark)}>{<FaSearch />}</div>
     </div>
   );
