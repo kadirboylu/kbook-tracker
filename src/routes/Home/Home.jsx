d27404722cc55a2542cc06e4da64cbee9a0555d8
Kadir Boylu <kadir.boylu@outlook.com>
2022-09-07 00:10:52 +0300

    Add book container

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,18 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { GiOpenBook } from "react-icons/gi";
+import clsx from "clsx";
 
 import styles from "./Home.module.scss";
 
-import { Search } from "@/components";
+import { Search, BookContainer } from "@/components";
 
 export const Home = () => {
+  const { query } = useSelector((state) => state.book);
+
+  const extraStyles = {
+    [styles.direction]: query !== "",
+  };
+
   return (
-    <div className={styles.base}>
-      <div className={styles.logo}>
-        <GiOpenBook className="dark:text-slate-800" />
-      </div>
+    <div className={clsx(styles.base, extraStyles)}>
       <div className="w-full">
-        <Search />
+        <div className={styles.logo}>
+          <GiOpenBook className="dark:text-slate-800" />
+        </div>
+        <div className="w-full">
+          <Search />
+        </div>
+      </div>
+      <div className="mt-10 w-full">
+        <BookContainer />
       </div>
     </div>
   );
