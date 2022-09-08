f086b575eb2b631b21fc949ff67a02ee6f678ce7
Kadir Boylu <kadir.boylu@outlook.com>
2022-09-08 14:57:31 +0300

    Import collection rows

--- src/routes/MyBooks/MyBooks.jsx
+++ src/routes/MyBooks/MyBooks.jsx
@@ -2,6 +2,15 @@ import React from "react";
 
 import styles from "./MyBooks.module.scss";
 
+import { CollectionRow } from "@/Components";
+
 export const MyBooks = () => {
-  return <div>MyBooks</div>;
+  return (
+    <div className="md:w-[90%] w-[75%] m-auto">
+      <CollectionRow category="Reading Now" />
+      <CollectionRow category="Have Read" />
+      <CollectionRow category="Books to read" />
+      <CollectionRow category="Wish List" />
+    </div>
+  );
 };
